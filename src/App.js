3270c9db55b957853b6c40ffb2ec296c3bcd7cac
Kurtis Kemple <kurtiskemple@gmail.com>
2019-02-11 14:35:57 -0500

    add GraphQL API for TODOs to app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,122 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Amplify, { graphqlOperation } from "aws-amplify";
+import { withAuthenticator, Connect } from "aws-amplify-react";
 
-import Amplify from 'aws-amplify';
-import awsConfig from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react';
+import "./App.css";
+
+import awsConfig from "./aws-exports";
+import * as mutations from "./graphql/mutations";
+import * as queries from "./graphql/queries";
+import * as subscriptions from "./graphql/subscriptions";
 
 Amplify.configure(awsConfig);
+
+const ListView = ({ todos }) => (
+  <div>
+    <h2>All Todos</h2>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          {todo.name} - {todo.description} - ({todo.id})
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+class AddTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      description: ""
+    };
+  }
+
+  handleChange(name, ev) {
+    this.setState({ [name]: ev.target.value });
+  }
+
+  async submit() {
+    const { onCreate } = this.props;
+    var input = {
+      name: this.state.name,
+      description: this.state.description
+    };
+
+    await onCreate({ input });
+    this.setState({ name: "", description: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          name="name"
+          placeholder="name"
+          onChange={ev => {
+            this.handleChange("name", ev);
+          }}
+          style={{
+            padding: "8px 16px",
+            margin: "5px"
+          }}
+        />
+        <input
+          name="description"
+          placeholder="description"
+          onChange={ev => {
+            this.handleChange("description", ev);
+          }}
+          style={{
+            padding: "8px 16px",
+            margin: "5px"
+          }}
+        />
+        <button
+          style={{
+            padding: "8px 16px",
+            margin: "5px"
+          }}
+          onClick={this.submit.bind(this)}
+        >
+          Add
+        </button>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Add Todo</h2>
+        <Connect mutation={graphqlOperation(mutations.createTodo)}>
+          {({ mutation }) => <AddTodo onCreate={mutation} />}
+        </Connect>
+
+        <Connect
+          query={graphqlOperation(queries.listTodos)}
+          subscription={graphqlOperation(subscriptions.onCreateTodo)}
+          onSubscriptionMsg={(prev, { onCreateTodo }) => {
+            return {
+              listTodos: {
+                items: [...prev.listTodos.items, onCreateTodo]
+              }
+            };
+          }}
+        >
+          {({ data: { listTodos }, loading, error }) => {
+            if (error) return <h3>Error</h3>;
+            if (loading || !listTodos) return <h3>Loading...</h3>;
+            return listTodos.items.length ? (
+              <ListView todos={listTodos ? listTodos.items : []} />
+            ) : (
+              <h3>No todos yet...</h3>
+            );
+          }}
+        </Connect>
       </div>
     );
   }
